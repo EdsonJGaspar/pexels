672b383502f35b3d9b2f05b6fbeb1cabb84181dc
EdsonJGaspar <edsonjoaogaspar21@gmail.com>
2026-07-28 17:56:43 +0100

    chore: Configuracoes e aplicacao do modo dark and light

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "@/components/web/buttom-toogle";
+
+export default function Home() {
+  return (
+    <main>
+      <header>
+        <ModeToggle />
+      </header>
+      <div>Hello world!</div>
+    </main>
+  );
+}
