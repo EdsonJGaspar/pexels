7a79e5ccb9fdaa8e968ab748b469eb473e4b9a5f
EdsonJGaspar <edsonjoaogaspar21@gmail.com>
2026-07-29 17:34:16 +0100

    feat: Banner implementacao

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
-import { ModeToggle } from "@/components/web/buttom-toogle";
+import Image from "next/image";
 
 export default function Home() {
-  return <main>Ola</main>;
+  return (
+    <main>
+      <section className="h-96 relative overflow-hidden">
+        <Image
+          src={"/pexels-kolkatarchobiwala.jpg"}
+          alt="Banner pexel"
+          fill
+          className="object-cover object-center brightness-50"
+        />
+        <div className="absolute inset-0 bg-black/35" />
+        <div className="absolute inset-0 flex flex-col items-center justify-center">
+          <h1 className="p-4 w-2xl text-center font-serif text-3xl md:text-4xl text-foreground drop-shadow-lg">
+            As melhores fotos de banco de imagens gratuitas, imagens e vídeos
+            livres de royalties compartilhados por criadores.
+          </h1>
+        </div>
+      </section>
+    </main>
+  );
 }
