4a577f32e1611a9a682e0a4a43c0c2ef542c326e
EdsonJGaspar <edsonjoaogaspar21@gmail.com>
2026-07-29 15:46:48 +0100

    feat: Terminado o side bar

--- components/web/app-sidebar.tsx
+++ components/web/app-sidebar.tsx
@@ -12,13 +12,26 @@ import {
   SidebarMenuItem,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
-import { Home } from "lucide-react";
+import {
+  Group,
+  Heart,
+  Home,
+  Image,
+  Navigation,
+  Settings,
+  User,
+  Video,
+} from "lucide-react";
 import { SidebarCollapseButtom } from "./sidebar-collapse-buttom";
+import { SidebarMenuItems } from "./sidebar-menu-items";
 
 export function AppSidebar() {
   return (
     <Sidebar collapsible="icon">
       <SidebarHeader>
+        <h1 className="group-data-[collapsible=icon]:hidden text-primary font-bold text-2xl font-heading p-3 md:p-6">
+          Pexels
+        </h1>
         <SidebarMenu>
           <SidebarCollapseButtom />
         </SidebarMenu>
@@ -30,24 +43,47 @@ export function AppSidebar() {
           </SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton
-                  render={
-                    <a href="#">
-                      <Home />
-                      <span className="group-data-[collapsible=icon]:hidden">
-                        Dashboard
-                      </span>
-                    </a>
-                  }
-                />
-              </SidebarMenuItem>
+              <SidebarMenuItems icon={Home} link="/" title="Home" />
+              <SidebarMenuItems
+                icon={Navigation}
+                link="/explore"
+                title="Explore"
+              />
+              <SidebarMenuItems icon={Image} link="/phots" title="Photos" />
+              <SidebarMenuItems icon={Video} link="/videos" title="Videos" />
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>
+            <span className="group-data-[collapsible=icon]:hidden">
+              Labriry
+            </span>
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItems
+                icon={Group}
+                link="/collections"
+                title="Collections"
+              />
+              <SidebarMenuItems
+                icon={Heart}
+                link="/favorites"
+                title="Favorites"
+              />
+              <SidebarMenuItems
+                icon={Settings}
+                link="/settings"
+                title="Settings"
+              />
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter>
-        <span className="group-data-[collapsible=icon]:hidden">Footer</span>
+      <SidebarFooter className="flex flex-row gap-2 items-center">
+        <User />
+        <span className="group-data-[collapsible=icon]:hidden">User</span>
       </SidebarFooter>
     </Sidebar>
   );
---
+++ components/web/header/header.tsx
@@ -0,0 +1,9 @@
+import { ModeToggle } from "../buttom-toogle";
+
+export function Header() {
+  return (
+    <header className="border">
+      <ModeToggle />
+    </header>
+  );
+}
